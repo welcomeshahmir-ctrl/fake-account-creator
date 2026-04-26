8b1775d90d8426a0838b875a4a9ae296909d16fd
welcomeshahmir-ctrl <welcomeshahmir@gmail.com>
2026-04-27 00:44:23 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+echo "Installing system dependencies..."
 apt-get update
 apt-get install -y chromium
+
+echo "Installing Playwright browsers..."
+playwright install
 playwright install chromium
